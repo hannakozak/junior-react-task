a9a40aa42a930972e0bf0adda6e8216ade00d491
Hanna Kozak <hannawkozak@gmail.com>
2022-09-12 22:26:56 +0100

    initial setup

---
+++ src/app/apolloClient.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+export const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+});
