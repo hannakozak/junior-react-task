86c2be5b0e85e39dae917a4e01ad107dc7f885ca
Hanna Kozak <hannawkozak@gmail.com>
2022-09-14 00:02:02 +0100

    feat: add routes to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/navigation/Navigation';
+import CategoryListingPage from './components/CategoryListingPage';
 
 export class App extends React.Component {
   render() {
     return (
-      <>
-        <h1> React Junior Test</h1>
-      </>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<CategoryListingPage />} />
+          <Route
+            path="/category/:categoryName"
+            element={<CategoryListingPage />}
+          />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
