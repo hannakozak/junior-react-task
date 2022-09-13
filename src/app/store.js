683d24e24d06268cfff0a5c0849b441e5ebeb300
Hanna Kozak <hannawkozak@gmail.com>
2022-09-13 23:58:46 +0100

    feat: update redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import categoriesSlice from '../features/categories/categoriesSlice';
+import categoryProductsSlice from '../features/categoryProducts/categoryProductsSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    categories: categoriesSlice,
+    categoryProducts: categoryProductsSlice
+  }
 });
