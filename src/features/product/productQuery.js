37813f2f4c522874dd38d3c064481dcb7959aa82
Hanna Kozak <hannawkozak@gmail.com>
2022-09-21 20:28:49 +0100

    feat: add product query

---
+++ src/features/product/productQuery.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+export const ProductQuery = gql`
+  query ($productId: String!) {
+    product(id: $productId) {
+      id
+      name
+      inStock
+      gallery
+      description
+      category
+      attributes {
+        id
+        name
+        type
+        items {
+          displayValue
+          value
+          id
+        }
+      }
+      prices {
+        currency {
+          label
+          symbol
+        }
+        amount
+      }
+      brand
+    }
+  }
+`;
